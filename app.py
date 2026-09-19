1d3af6ef99e336827562915935b76af260638a52
Luleka Ntakana <luleka0306@gmail.com>
2026-09-19 14:14:50 +0200

    Installed pypdf to enable PDF text extration

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
+from pypdf import PdfReader
 import streamlit as st
 
 st.title("LegalLens")
 st.write("AI-powered Legal Document Analysis Tool")
+
+uploaded_file = st.file_uploader(
+    "Upload a contract",
+    type=["pdf"] )
+
+if uploaded_file is not None:
+    reader = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() or ""
+
+    st.subheader("Contract Text")
+    st.text_area("Extracted text", text, height=400)
